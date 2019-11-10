d3886cc295088eaa7941b6bba2b1d4b7cd9be8ba
Randy <randypriv@gmail.com>
2019-11-10 11:37:08 +0800

    update

--- theme/Root.vue
+++ theme/Root.vue
@@ -7,6 +7,9 @@
         2014-2019
         <a href="/">Randy's Blog</a>
       </div>
+      <div class="mt-2">
+        可通過 <a class="border-b-4" href="/rss.xml">RSS</a> 或 <a class="border-b-4" href="https://tinyletter.com/randysblog">Email</a> 訂閱本博客
+      </div>
     </div>
   </div>
 </template>
