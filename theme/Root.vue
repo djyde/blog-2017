5aa6b0484099ad7c2b95443d174b3a6455e725ad
Randy <randypriv@gmail.com>
2020-03-08 13:42:28 +0800

    update

--- theme/Root.vue
+++ theme/Root.vue
@@ -3,7 +3,7 @@
     <slot />
 
     <div class="font-serif text-sm text-center p-12 sm:p-32 text-gray-500 font-bold">
-      <!-- <div>
+      <div>
         2014-2019
         <a href="/">Randy's Blog</a>
       </div>
@@ -11,7 +11,7 @@
         可通過
         <a class="border-b-4" href="/rss.xml">RSS</a> 或
         <a class="border-b-4" href="https://tinyletter.com/randysblog">Email</a> 訂閱本博客
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
